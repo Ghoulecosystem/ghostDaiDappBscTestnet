06fb54725e443b7a6ebe38d310ddf9b87238be0e
blockifi <samuelblizy@hotmail.com>
2021-11-25 10:54:28 +0100

    update 25/11/21

--- src/views/vaults.vue
+++ src/views/vaults.vue
@@ -1,8 +1,19 @@
 <template>
 <div>
 <topmenu></topmenu>
+<div class="flex flex-col flex-1 items-center justify-start w-screen overflow-y-auto overflow-x-hidden z-0 pt-4 sm:pt-8 px-4 md:pt-10 pb-20">
+    <div class="sc-gwVKww sc-daURTG gBShpO">
 
-  <div class="container max-w-5xl mx-auto space-y-2">
+    </div>
+    <div height="0" class="sc-cQFLBn cThFpX">
+        <div class="sc-gojNiO qxEgk"></div>
+    </div>
+   
+<div class="absolute top-100 right-0 left-50 overflow-hidden" style="z-index: -1;"></div>
+
+<div class="container max-w-3xl mx-auto px-4 md:px-0">
+    <!-- <vault v-if="user.address && chainID == '97'"></vault> -->
+      <div  v-if="user.address && chainID == '97'" class="container max-w-5xl mx-auto space-y-2">
       <div class="loader overlay"  v-show="mainloading">
           <img :src="logo" class="rotate" width="100" height="100" />
        </div>
@@ -98,6 +109,37 @@
         
 </div>
   </div>
+ <div v-if="user.address && chainID != '97'" class="relative bg-dm-tertiary" style="border-radius: 10px; background-image: url(&quot;&quot;); background-repeat: no-repeat; background-size: contain; background-position: center bottom;">
+        <div>
+            
+            <div class="px-2 py-4 sm:p-8">
+               
+                
+                    <div class="flex-col space-y-2">
+                        <div class="w-full text-center py-6">
+                            <h2 class="alert alert-danger" style="text-align:center; color : black">Wrong Network!!! Please connect to BSC Testnet Network!</h2></div></div>
+                   
+        </div>
+    </div>
+</div>
+    <div v-if="!user.address " class="relative bg-dm-tertiary" style="border-radius: 10px; background-image: url(&quot;&quot;); background-repeat: no-repeat; background-size: contain; background-position: center bottom;">
+        <div>
+            
+            <div class="px-2 py-4 sm:p-8">
+               
+                
+                    <div class="flex-col space-y-2">
+                        <div class="w-full text-center py-6">
+                            <h2 class="" style="text-align:center; color : white">Please connect your wallet!</h2></div></div>
+                   
+        </div>
+    </div>
+</div>
+
+
+</div>
+</div>
+
 </div>
 </template>
 <script>
